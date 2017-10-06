6c5fc022394b666e82c02a548b24daa4ae80cc6c
thepragz <thepragz@gmail.com>
2017-10-06 18:34:40 +0800

    added css and js file

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
-<!doctype html>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Awesome Theme</title>
+<meta charset="utf-8">
+
+<title>Awesome Theme</title>
+<link rel="stylesheet" id="customstyle-css"  type="text/css" href="http://localhost/wordpress/wp-content/themes/awesometheme/css/awesome.css?ver=1.0.0" media="all">
+<?php wp_head(); ?>
+
+
 </head>
 <body>
-
-
-<!-- looped at footer.php -->
-<!-- </body>
-</html> -->
