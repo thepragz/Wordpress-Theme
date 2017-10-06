6776247b16582efabec6f3843f449156579d81dd
thepragz <thepragz@gmail.com>
2017-10-06 16:34:54 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Awesome Theme</title>
+</head>
+<body>
+
+
+<!-- looped at footer.php -->
+<!-- </body>
+</html> -->
